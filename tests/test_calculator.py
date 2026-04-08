d711c404656e1faac73950ac182a5b6b66f30a3b
ysanne617 <ystian617@gmail.com>
2026-04-08 12:03:04 +0800

    test: Add unit tests for Calculator subtract method

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -10,3 +10,9 @@ class TestCalculator:
         assert self.calculator.add(-1, 1) == 0
         assert self.calculator.add(0, 0) == 0
         assert self.calculator.add(-5, -5) == -10
+
+    def test_subtract(self):
+        assert self.calculator.subtract(5, 2) == 3
+        assert self.calculator.subtract(1, -1) == 2
+        assert self.calculator.subtract(0, 0) == 0
+        assert self.calculator.subtract(-5, -5) == 0
